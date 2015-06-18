f5016d6c29513d18764256a61cbc170db26ae8f4
Alexander Graefe <alex@codemo.de>
2015-06-18 12:16:30 +0200

    initial src commit

---
+++ src/main/java/com/rickenharp/javastuff/App.java
@@ -0,0 +1,13 @@
+package com.rickenharp.javastuff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
